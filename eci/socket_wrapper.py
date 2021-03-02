0208ef60370baf4c4a6414c5db89d39a06079beb
Joshua Teves <joshua.teves@nih.gov>
2021-03-02 14:31:43 -0500

    Adds to socket wrapper

--- eci/socket_wrapper.py
+++ eci/socket_wrapper.py
@@ -8,54 +8,93 @@ class Socket():
     """
     Wrapper for the built-in socket() class, to simplify communication
     """
+    buffersize = 4096
+    mode = 'rwb'
 
-    def connect(self, str_address, port_no):
+    def __init__(self, address: str, port: int):
         """
-        Connect to the amp at given ip and port
+        Construct Socket object; does not connect.
+
+        Parameters
+        ----------
+        address: str
+            The IPv4 address the socket will use
+        port: int
+            The port number the socket will use
+        """
+        self._address = (address, port)
+        self._socket = None
+
+
+    def connect(self):
+        """
+        Connect to the TCP/IP socket
         """
 
-        self._socket = socket.socket( socket.AF_INET, # IP_V4
-                                      socket.SOCK_STREAM 
-                                    )
-        self._socket.connect(( str_address, port_no ))     
+        self._socket = socket.socket( socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect( self._address )
 
-        self._connection = self._socket.makefile('rwb', 0) # read and write, no internal buffer     
   
     def disconnect(self):
         """
-        Disconnect from amp
+        Disconnect from the socket
         """
-        self._connection.close()
         self._socket.close()
-        del self._connection
-        del self._socket
+        self._socket = None
 
     def write(self, data):
         """
         Write to the amp (send data)
         """
-        self._connection.write(data)
+        if not self._socket:
+            raise RuntimeError('You have not connected')
+        response = self._socket.send(data)
+        if not response:
+            raise RuntimeError('Connection Dropped')
+        return response
     
-    def read(self, size = -1):
+    def read(self):
         """
         Read data from amp. BLOCKS ON READING
         """
-        if size < 0 :
-            return self._connection.read()
-        else :
-            return self._connection.read( size )
+        if not self._socket:
+            raise RuntimeError('You have not connected')
+        return self._socket.recv(Socket.buffersize)
 
 class UDP():
     """
     Wrapper for UDP protocol used in NTP synchronization
+
+    Attributes
+    ----------
+    _address: tuple
+        The representation of IPv4 address and port number
+    _socket: socket.socket
+        The socket to communicate with
     """
+
+    def __init__(self, ip: str, port: int) -> None:
+        """
+        Construct a UDP object; does not connect
+        
+        Parameters
+        ----------
+        ip: str
+            The IPv4 address to connect to
+        port: int
+            The port number to connect with
+        """
+        self._address = (ip, port)
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._bound = False
+
     
-    def connect(self, str_addr):
+    def connect(self):
         """
         Establish a UDP connection with amp
         """
-        self._str_addr = str_addr
-        self._socket = socket.socket( socket.AF_INET, socket.SOCK_DGRAM)
+        self._socket.bind(self._address)
+        self._bound = True
 
     def disconnect(self):
         """
@@ -67,10 +106,14 @@ class UDP():
         """
         Write a datagram to amp
         """
-        self._sent = self._socket.sendto(data, self._str_addr)
+        if not self._bound:
+            raise RuntimeError('UDP socket not bound')
+        sent = self._socket.sendto(data, self._address)
+        return sent
     
-    def read(self, size=-1):
+    def read(self):
         """
         Read response from amp.
         """
-        self._rx_data, self._server = self._socket.recvfrom(4096)
+        rx_data, _ = self._socket.recvfrom(4096)
+        return rx_data
