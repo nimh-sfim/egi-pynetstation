1587fa95520022c2f2d237e644ae2730112610c4
Joshua Teves <joshua.teves@nih.gov>
2021-11-15 17:17:13 -0500

    Modify version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='eci',
-    version='0.0.1',
+    version='0.0.1a1',
     author='Joshua B. Teves',
     author_email='joshua.teves@nih.gov',
     description='A library for using EGI EEG network interface',
